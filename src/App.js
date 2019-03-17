9072cf462768de9417e489e662a1ab953da15558
Max <mjann@ucsc.edu>
2019-03-16 19:08:52 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import griff from './griffin.MP4';
+
 import './App.css';
 
 class App extends Component {
   render() {
+    const mySteez = {
+      "width":"50%",
+    }
+    const mySteez2 = {
+      "width":"100%",
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+        <p>
+            J Griff
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <video src={griff} autoPlay="on" type="video/mp4" style={mySteez2}></video>
+        
+        <a href="https://maxjann.com" class="robo">Jann Software</a>
       </div>
     );
   }
